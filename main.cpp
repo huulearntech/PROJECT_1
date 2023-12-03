4b171d7214c181dd689a8f1fb6c11af070cb9c6c
trinh <trinh@DESKTOP-K7MBFAN>
2023-12-03 08:59:30 +0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+int main(int argc, char** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	TTF_Init();
+
+	SDL_Delay(3000);
+	SDL_Quit();
+	return 0;
+}
